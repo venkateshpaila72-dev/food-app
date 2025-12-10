359c0eddcd45b1c8b3a4622da2395a2a20a9801e
venkateshpaila <venkateshpaila72@gmail.com>
2025-12-10 14:27:44 +0530

    continued after error fixed

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import './FoodDisplay.css'
+import { StoreContext } from '../../context/StoreContext';
 const FoodDisplay = (category) => {
     
     const {food_list}=useContext(StoreContext);
